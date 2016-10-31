0a80308db2cdd56b80898c5092c2b983ef4cfdc5
Nepipenkoigor <igornepipenko@gmail.com>
2016-10-31 19:27:09 +0200

    prepare lesson-3

---
+++ lesson-3/flickr/fetch.ts
@@ -0,0 +1,8 @@
+
+// https://developer.mozilla.org/en-US/docs/Web/API/GlobalFetch/fetch
+
+//`${this.uri}method=${this.qyeryMethod}&api_key=${this.apiKey}&text=${text}&page=1&format=json&nojsoncallback=1`
+//https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg
+// uri: 'https://api.flickr.com/services/rest/?',
+//     queryMethod: 'flickr.photos.search',
+//     apiKey: '7fbc4d0fd04492d32 fa9a2f718c6293e'
